ad00602b912e6202d6359a74497331ad1a0d854e
Taylor Galloway <galloway.taylor@gmail.com>
2023-11-28 18:29:05 -0600

    Add test data

---
+++ src/testData.js
@@ -0,0 +1,23 @@
+const folders = [
+  {
+    id: 1,
+    name: "Ruby Methods"
+  },
+  {
+    id: 2,
+    name: "SQL"
+  }
+]
+
+const flashcards = [
+  {
+    id: 1,
+    front: "Inner Join syntax example",
+    back: "select * from users inner join tasks on users.id = tasks.id"
+  },
+  {
+    id: 1,
+    front: "Inner Join syntax example",
+    back: "select * from users inner join tasks on users.id = tasks.id"
+  }
+]
