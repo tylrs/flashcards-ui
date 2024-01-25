bb7f1eb788fbb8e622edaea2be65d136ea019ade
Taylor Galloway <galloway.taylor@gmail.com>
2024-01-24 18:11:51 -0600

    Add folder icon

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import FolderIcon from "../../assets/svgs/iconFolder.svg?react"
 
 const Deck = ({ deckData }) => {
   const { name } = deckData
 
   return (
-    <div className="border border-solid border-black text-center">
+    <div className="flex flex-col items-center border border-solid border-black text-center py-10 cursor-pointer">
+      <FolderIcon />
       <p>{name}</p>
     </div>
   )
