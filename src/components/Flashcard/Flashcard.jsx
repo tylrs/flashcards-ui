96549db5ff22bf2fd5b9ba30f95c878c5bd8878a
Taylor Galloway <galloway.taylor@gmail.com>
2024-01-25 17:03:51 -0600

    Fix text x spacing on flashcard

--- src/components/Flashcard/Flashcard.jsx
+++ src/components/Flashcard/Flashcard.jsx
@@ -5,12 +5,12 @@ const Flashcard = ({ cardData }) => {
 
   return (
     <div className="flex justify-around border-solid border-1px border border-black">
-      <div className="text-center pt-2 pb-6">
+      <div className="text-center pt-2 pb-6 px-6">
         <h3 className="text-1xl">Front</h3>
         <p className="text-2xl">{front}</p>
       </div>
       <div className="border border-solid border-black h-full" />
-      <div className="text-center pt-2 pb-6">
+      <div className="text-center pt-2 pb-6 px-6">
         <h3 className="">Back</h3>
         <p className="text-2xl">{back}</p>
       </div>
