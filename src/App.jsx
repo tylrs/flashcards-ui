fbc2c708da488fc3827c8c8fda76208e0df8a676
Taylor Galloway <galloway.taylor@gmail.com>
2024-01-24 18:30:29 -0600

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css"
+import React from "react"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Homepage from "./pages/Homepage"
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  }
+]);
+
+const App = () => {
   return (
-    <div className="w-full h-screen p-10">
-      <Homepage />
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
